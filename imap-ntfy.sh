f64a9694f749343e4f23c4fc8f56463efa92893e
seb1k <64972483+seb1k@users.noreply.github.com>
2024-04-07 16:13:34 +0200

    work with empty subject

--- imap-ntfy.sh
+++ imap-ntfy.sh
@@ -30,7 +30,6 @@ if [ ! "$user" ]; then
 
 
 	#Start this script into pipe to hide user password + background + nohup (except user email)
-	#start_script $1 $2 $3 $4
 	echo "Start script"
 	echo "$1 $2 $3 $4" | (nohup $0 $1 &)
 
@@ -50,6 +49,9 @@ fi
 PIDopenssl=-1
 SENDER=""
 SUBJECT=""
+PREVIOUSUID=""
+SUBJECT2=""
+SUBJECT2ok=false
 
 
 start_idle () {
@@ -66,40 +68,55 @@ start_idle () {
 }
 
 
-try_send_ntfy_info_in2s()
+try_send_notif()
 {
-sleep 2
-try_send_ntfy_info
+if [ "$PIDfunc1s" ]; then
+	kill $PIDfunc1s
+	PIDfunc1s=""
+fi
+
+send_ntfy_info_1s &
+PIDfunc1s=$(echo $!)
+}
+
+send_ntfy_info_1s()
+{
+sleep 1
+send_ntfy_info
 echo ". idle" > /proc/$PIDopenssl/fd/0
 }
 
-try_send_ntfy_info()
+send_ntfy_info()
 {
-if [ ! -z "$SENDER" ] && [ ! -z "$SUBJECT" ];then
-	echo curl -s -H "Tags: envelope" -H "Title: $SENDER" -H "Click: https://test.site/alink" -H "m: $SUBJECT" -d "" $ntfy_topic
-
-	curl -s -H "Tags: envelope" -H "Title: $SENDER" -H "Click: https://test.site/alink" -H "m: $SUBJECT" -d "" $ntfy_topic
-	SENDER=""
-	SUBJECT=""
-	
+if  [[ ! "$SUBJECT" ]];then
+	SUBJECT="(no subject)"
 fi
+
+echo curl -s -H "Tags: envelope" -H "Title: $SENDER" -H "Click: https://test.site/alink" -H "m: $SUBJECT" -d "" $ntfy_topic
+curl -s -H "Tags: envelope" -H "Title: $SENDER" -H "Click: https://test.site/alink" -H "m: $SUBJECT" -d "" $ntfy_topic
+
 }
 
 
 # Start ssl connection
-echo "Starting imap-notify, logging in as $user at $server"
+echo "Starting imap-notify"
+sleep 3 # wait for VM network
+echo "Logging in as $user at $server"
 
 while :
 do
 
 	echo "Connection..."
+	PIDopenssl=-1
 
 	while read -r line ; do
 	  # Debug info, turn this off for silent operation
 	  #echo "----------------------------------------"
 	  #echo "$line"
-	 [ $PIDopenssl = -1 ]
-		PIDopenssl=$(ps -ef | grep -v grep|grep $$ | grep openssl| awk '{print $2}')
+	  
+	  #echo "NEW mail SUBJECT2ok : $SUBJECT2ok"
+
+	 [ $PIDopenssl = -1 ] && PIDopenssl=$(ps -ef | grep -v grep|grep $$ | grep openssl| awk '{print $2}')
 		
 	  if echo "$line" | grep -Eq ". [1-9][0-9]? EXISTS"; then
 		#echo "New mail received, executing $command"
@@ -109,17 +126,23 @@ do
 	  fi
 
 	  if echo "$line" | grep "FETCH (UID"; then
-		LASTUID=$(echo $line | grep -o -P '(?<=UID ).*(?=\))')
-		echo ". uid FETCH $LASTUID (BODY.PEEK[HEADER.FIELDS (FROM SUBJECT)])" > /proc/$PIDopenssl/fd/0
+		LASTUID=$(echo $line | grep -o -P '(?<=UID ).*(?=\))' | cut -d ' ' -f 1)
+		#echo "NEW LASTUID : $LASTUID"
+					
+		if [[ "$LASTUID" != "$PREVIOUSUID" ]];then
+			echo ". uid FETCH $LASTUID (BODY.PEEK[HEADER.FIELDS (FROM SUBJECT)])" > /proc/$PIDopenssl/fd/0
+			SUBJECT=""
+			SUBJECT2=""
+			SUBJECT2ok=false
+			PREVIOUSUID=$LASTUID
+		fi
 	  fi
-
-	 
 
 
 
 
 	  if echo "$line" | grep "From: "; then
-		echo " MAILFrom : $line"
+		#echo "MAILFrom : $line"
 		
 		SENDER=$(echo $line| cut -c 6-)
 			
@@ -129,41 +152,32 @@ do
 		
 		SENDER=$(echo $SENDER | xargs)
 
-		try_send_ntfy_info
+		try_send_notif
 		
 	  elif echo "$line" | grep "Subject: "; then
-		echo "MAILSubject :$line"
-		SUBJECT=$(echo $line| cut -c 9- )
+		#echo "MAILSubject :$line"
+		SUBJECT=$(echo $line|cut -c 9-|tr -d '\r')
+		
+		#echo "Subject1 :$SUBJECT"
+		
+		try_send_notif
 
-		##### TRY SEND EMAIL when subject is full (wait 2s to get all subject) #####
-		try_send_ntfy_info_in2s &
-		PIDfunc2s=$(echo $!)
-	  elif [ "$SUBJECT" ] ;then
-		 echo "MAILSubject (multiline) : $line"
-		 # Fill up rest of subject
-		 
-		 #line2=$(echo $line | tr -d '\r' )
-		 #SUBJECT="$SUBJECT$line2"
-		 SUBJECT=$(echo "$SUBJECT$line" | tr -d '\r' )
-		 
-		 if [ "$PIDfunc2s" ]; then
-			 kill $PIDfunc2s
-			 echo ". idle" > /proc/$PIDopenssl/fd/0
-			 
-			try_send_ntfy_info
-			PIDfunc2s=""
-		 else
-			echo "ERROR - NO PIDfunc2s, clear subject : $SUBJECT"
-			SUBJECT=""
-		 fi
+	  elif [[ "$SUBJECT" ]] && [[ "$SUBJECT2ok" = false ]]; then
+		#echo "MAILSubject (multiline) : $line"
+
+		SUBJECT2=$(echo "$line" | tr -d '\r' )
+		SUBJECT2ok=true
+		#echo "Subject2 :$SUBJECT2"
+		
+		try_send_notif
+
 	  fi
-
 
 
 	done < <(openssl s_client -crlf -quiet -connect "$server" 2>/dev/null < <(start_idle))
 
 
-	# Disconnected ? start reconnexion
+	# Disconnected ? start reconnection
 
 	sleep 60
 
